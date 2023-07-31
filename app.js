4078735ca3dbd7e400065cdb3896e30bcb14950e
chiliexe <chiliexe_2015@hotmail.com>
2023-07-31 14:44:49 -0300

    fix request timing response

--- app.js
+++ app.js
@@ -6,13 +6,13 @@ const port = 5500;
 
 const inactiveUserAndVehicle = new InactiveUserAndVehicle();
 
-app.get('/softruck/inactive/user-and-vehicle', (req, res) => {
-    inactiveUserAndVehicle.execute();
-    res.status(200);
+app.get('/softruck/inactive/user-and-vehicle', async (req, res) => {
+  res.send("OK").status(200);
+  await inactiveUserAndVehicle.execute();
 })
 
 
 app.listen(port, () => {
-  console.log(`Server Up: http://localhost:5500/softruck/inactive/user-and-vehicle`)
+  console.log(`Server Up: /softruck/inactive/user-and-vehicle`)
 })
 
--- controllers/InactiveUserAndVehicle.js
+++ controllers/InactiveUserAndVehicle.js
@@ -1,58 +1,66 @@
-const puppeteer = require('puppeteer')
-const Softruck = require('../util/Softruck')
-const MyGoogleSheets = require('../util/MyGoogleSheets')
+const puppeteer = require("puppeteer");
+const Softruck = require("../util/Softruck");
+const MyGoogleSheets = require("../util/MyGoogleSheets");
 
 const softruck = new Softruck();
 const myGoogleSheets = new MyGoogleSheets();
 
 class InactiveUserAndVehicle {
+  /**
+   * SHEETS LINKS
+   * https://docs.google.com/spreadsheets/d/1FzoybRtHtHx-xb17hdbqXjRElN-OOoxB6ihR22DEHhE/edit#gid=0
+   */
 
-    /**
-     * SHEETS LINKS 
-     * https://docs.google.com/spreadsheets/d/1FzoybRtHtHx-xb17hdbqXjRElN-OOoxB6ihR22DEHhE/edit#gid=0
-     */
+  async execute() {
+    try {
+      console.log("Executing...");
+      // GET DATA FROM GOOGLE SHEETS
+      const response = await myGoogleSheets.getDataFromSheets(
+        "Inativar usuário/veículo"
+      );
 
-    async execute(){
-        try {
-          console.log("Executing...")
-            // GET DATA FROM GOOGLE SHEETS
-            const response = await myGoogleSheets.getDataFromSheets("Inativar usuário/veículo")
+      // FILTER DATA FROM SHEETS
+      const values = response.data.values;
+      const active = values[0][1];
 
-            // FILTER DATA FROM SHEETS
-            const values = response.data.values;
-            const active = values[0][1];
-            
-            if(active != "SIM") return;
-            
-            let data = []
-            values.forEach((value, index) => {
-              if(index > 2) {
-                data.push({username: value[0], plate: value[1], done: value[2]})
-              }
-            })
+      if (active != "SIM") return;
 
-            // INIT PUPPETEER
-            const browser = await puppeteer.launch({headless: "new", defaultViewport: { height: 600, width: 1200 }})
-            const page = await browser.newPage()
-            await softruck.doLogin(page);
-            
-            //LOOP - DISABLE USER -> DISABLE VEHICLE -> UPDATE CELL FROM SHEETS
-            let index = 4;
-            for(const value of data){
-              if(value.done == "on"){ index++; continue }
-              await softruck.disableUser(page, value.username)
-              await softruck.disableVehicle(page, value.plate)
-              await myGoogleSheets.updateDataFromSheets(`Inativar usuário/veículo!C${index}`, "on");
-              index++;
-            }
-            
-            // CLOSE BROWSER
-            await browser.close();
-            console.log("Finished...")
-        } catch (error) {
-            this();
+      let data = [];
+      values.forEach((value, index) => {
+        if (index > 2) {
+          data.push({ username: value[0], plate: value[1], done: value[2] });
         }
+      });
+
+      // INIT PUPPETEER
+      const browser = await puppeteer.launch({
+        headless: "new",
+        defaultViewport: { height: 600, width: 1200 },
+      });
+      const page = await browser.newPage();
+      await softruck.doLogin(page);
+
+      //LOOP - DISABLE USER -> DISABLE VEHICLE -> UPDATE CELL FROM SHEETS
+      let index = 4;
+      for (const value of data) {
+        if (value.done == "on") {
+          index++;
+          continue;
+        }
+        await softruck.disableUser(page, value.username);
+        await softruck.disableVehicle(page, value.plate);
+        await myGoogleSheets.updateDataFromSheets(`Inativar usuário/veículo!C${index}`, "on");
+        index++;
+      }
+
+      // CLOSE BROWSER
+      await browser.close();
+      console.log("Finished...");
+      return;
+    } catch (error) {
+      this.execute();
     }
+  }
 }
 
 module.exports = InactiveUserAndVehicle;
